81857f51e5ee3275e4a53958103876b8f46e325e
charlesangus <c.angustaylor@gmail.com>
2026-09-15 12:00:55 -0400

    One helper header for the metadata plugins' pixel copy and value text; convert Print, View and Compare

--- Support/Plugins/MetadataCompare/metadataCompare.cpp
+++ Support/Plugins/MetadataCompare/metadataCompare.cpp
@@ -6,7 +6,6 @@
 #include <windows.h>
 #endif
 
-#include <cstring>
 #include <memory>
 #include <sstream>
 #include <string>
@@ -14,6 +13,8 @@
 
 #include "ofxsImageEffect.h"
 #include "ofxsMetadata.h"
+
+#include "../include/ofxsPixelCopy.H"
 
 namespace {
 
@@ -26,68 +27,12 @@ namespace {
   const char kMaskOnlyPrefix[]   = "Mask only: ";
   const char kDiffersPrefix[]    = "differs: ";
 
-  /** @brief every value of a key, comma separated, read back as the type the host holds it as */
-  std::string valueText(const OFX::MetadataSet &metadata, const OFX::MetadataEntry &entry)
-  {
-    std::ostringstream text;
-
-    for(int i = 0; i < entry.dimension; i++) {
-      if(i)
-        text << ",";
-
-      switch(entry.type) {
-      case OFX::eMetadataTypeInt    : text << metadata.getInt(entry.key, i); break;
-      case OFX::eMetadataTypeDouble : text << metadata.getDouble(entry.key, i); break;
-      default                       : text << metadata.getString(entry.key, i); break;
-      }
-    }
-
-    return text.str();
-  }
-
   void appendLine(std::string &text, const std::string &line)
   {
     if(!text.empty())
       text += "\n";
 
     text += line;
-  }
-
-  int bytesPerPixel(const OFX::Image &image)
-  {
-    int perComponent = 0;
-
-    switch(image.getPixelDepth()) {
-    case OFX::eBitDepthUByte  : perComponent = 1; break;
-    case OFX::eBitDepthUShort : perComponent = 2; break;
-    case OFX::eBitDepthHalf   : perComponent = 2; break;
-    case OFX::eBitDepthFloat  : perComponent = 4; break;
-    default : return 0;
-    }
-
-    return perComponent * image.getPixelComponentCount();
-  }
-
-  void copyPixels(const OFX::Image &src, OFX::Image &dst, const OfxRectI &window)
-  {
-    const int pixelBytes = bytesPerPixel(dst);
-
-    if(pixelBytes == 0 || pixelBytes != bytesPerPixel(src))
-      OFX::throwSuiteStatusException(kOfxStatErrImageFormat);
-
-    for(int y = window.y1; y < window.y2; y++) {
-      for(int x = window.x1; x < window.x2; x++) {
-        void *to = dst.getPixelAddress(x, y);
-
-        if(!to)
-          continue;
-
-        if(const void *from = src.getPixelAddress(x, y))
-          memcpy(to, from, size_t(pixelBytes));
-        else
-          memset(to, 0, size_t(pixelBytes));
-      }
-    }
   }
 
 }
--- Support/Plugins/MetadataView/metadataView.cpp
+++ Support/Plugins/MetadataView/metadataView.cpp
@@ -6,8 +6,6 @@
 #include <windows.h>
 #endif
 
-#include <cctype>
-#include <cstring>
 #include <memory>
 #include <sstream>
 #include <string>
@@ -15,6 +13,8 @@
 
 #include "ofxsImageEffect.h"
 #include "ofxsMetadata.h"
+
+#include "../include/ofxsPixelCopy.H"
 
 namespace {
 
@@ -28,11 +28,6 @@ namespace {
     eFilterModeValuesOnly
   };
 
-  char lowerCase(char c)
-  {
-    return char(tolower((unsigned char) c));
-  }
-
   /** @brief does text hold needle, ignoring case? An empty needle is held by anything */
   bool containsNoCase(const std::string &text, const std::string &needle)
   {
@@ -42,7 +37,7 @@ namespace {
     for(size_t at = 0; at + needle.size() <= text.size(); at++) {
       size_t i = 0;
 
-      while(i < needle.size() && lowerCase(text[at + i]) == lowerCase(needle[i]))
+      while(i < needle.size() && OFX::lowerCase(text[at + i]) == OFX::lowerCase(needle[i]))
         i++;
 
       if(i == needle.size())
@@ -50,62 +45,6 @@ namespace {
     }
 
     return false;
-  }
-
-  /** @brief every value of a key, comma separated, read back as the type the host holds it as */
-  std::string valueText(const OFX::MetadataSet &metadata, const OFX::MetadataEntry &entry)
-  {
-    std::ostringstream text;
-
-    for(int i = 0; i < entry.dimension; i++) {
-      if(i)
-        text << ",";
-
-      switch(entry.type) {
-      case OFX::eMetadataTypeInt    : text << metadata.getInt(entry.key, i); break;
-      case OFX::eMetadataTypeDouble : text << metadata.getDouble(entry.key, i); break;
-      default                       : text << metadata.getString(entry.key, i); break;
-      }
-    }
-
-    return text.str();
-  }
-
-  int bytesPerPixel(const OFX::Image &image)
-  {
-    int perComponent = 0;
-
-    switch(image.getPixelDepth()) {
-    case OFX::eBitDepthUByte  : perComponent = 1; break;
-    case OFX::eBitDepthUShort : perComponent = 2; break;
-    case OFX::eBitDepthHalf   : perComponent = 2; break;
-    case OFX::eBitDepthFloat  : perComponent = 4; break;
-    default : return 0;
-    }
-
-    return perComponent * image.getPixelComponentCount();
-  }
-
-  void copyPixels(const OFX::Image &src, OFX::Image &dst, const OfxRectI &window)
-  {
-    const int pixelBytes = bytesPerPixel(dst);
-
-    if(pixelBytes == 0 || pixelBytes != bytesPerPixel(src))
-      OFX::throwSuiteStatusException(kOfxStatErrImageFormat);
-
-    for(int y = window.y1; y < window.y2; y++) {
-      for(int x = window.x1; x < window.x2; x++) {
-        void *to = dst.getPixelAddress(x, y);
-
-        if(!to)
-          continue;
-
-        if(const void *from = src.getPixelAddress(x, y))
-          memcpy(to, from, size_t(pixelBytes));
-        else
-          memset(to, 0, size_t(pixelBytes));
-      }
-    }
   }
 
 }
